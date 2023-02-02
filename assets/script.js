29113e206d12f105f95d5346258b6e7f16ea5c39
rebecca wood <rebeccaseverowood@gmail.com>
2023-02-03 10:05:45 +1300

    Added IDs, added JS functionality linking API to button click

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,25 @@
-alert('Page working')
+const submitBtn = $('#search-btn')
+const clearBtn = $('#clear-btn')
+const form = $('#form')
+const searchTerm = $('#search-term');
+const startYear = $('#start-year');
+const endYear = $('#end-year');
+const articles = $('#articles');
+const queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + searchTerm + '&api-key=' + 'keuWGlK6emxzaXnr5oUXubBKvJlLIrUo';
+
+submitBtn.on('click', function () {
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+       })
+       .then(function(response){
+        console.log(response);
+        const articleP = $('<p>');
+        articleP.text(JSON.stringify(response));
+        articles.append(articleP);
+       })
+
+
+})
+
