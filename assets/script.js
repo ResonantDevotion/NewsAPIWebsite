e22f14912e1f1b0698c1da694f3b37fccf669fb4
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-02-03 11:19:13 +0000

    Delegate  event handling of submitBtn to its form parent, using event listener instead

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,41 @@
-const submitBtn = $('#search-btn')
-const clearBtn = $('#clear-btn')
-const form = $('#form')
-const searchTerm = $('#search-term');
-const startYear = $('#start-year');
-const endYear = $('#end-year');
-const articles = $('#articles');
-const queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + searchTerm + '&api-key=' + 'keuWGlK6emxzaXnr5oUXubBKvJlLIrUo';
+const submitBtn = $("#search-btn");
+const clearBtn = $("#clear-btn");
+const form = $("#form");
+const searchTerm = $("#search-term");
+const startYear = $("#start-year");
+const endYear = $("#end-year");
+const articles = $("#articles");
+const queryURL =
+  "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+  searchTerm +
+  "&api-key=" +
+  "keuWGlK6emxzaXnr5oUXubBKvJlLIrUo";
 
-submitBtn.on('click', function () {
+/*
+submitBtn.on("click", function () {
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    const articleP = $("<p>");
+    articleP.text(JSON.stringify(response));
+    articles.append(articleP);
+  });
+});
+*/
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-       })
-       .then(function(response){
-        console.log(response);
-        const articleP = $('<p>');
-        articleP.text(JSON.stringify(response));
-        articles.append(articleP);
-       })
+// * Delegate handling of submitBtn to the form (parent)
+form.on("submit", submitBtn, function (event) {
+  event.preventDefault();
 
-
-})
-
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    const articleP = $("<p>");
+    articleP.text(JSON.stringify(response));
+    articles.append(articleP);
+  });
+});
